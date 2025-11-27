dc60fa7d621e6fe2a5d23c7977374bd8a5934861
eggduzao <eggduzao@gmail.com>
2025-11-27 18:52:12 -0300

    chore:test-external-data

--- Results/hif/motif_match/motif_analysis.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-#rgt-motifanalysis --matching --normalize-bitscore --fpr=0.001 --organism mm9 --rand-proportion 1 motif_analysis_EM.txt
-
-rgt-motifanalysis --matching --normalize-bitscore --fpr=0.001 --organism mm9 --rand-proportion 0.00001 background_EM.txt
-
-
